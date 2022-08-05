9dd70857cb81856780387916efac235ec38ed2ba
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2022-08-05 21:40:01 +0800

    Use RawText instead of String in command
    Update SDK
    Add CMathParser source

--- Calculator/Command.cpp
+++ Calculator/Command.cpp
@@ -13,43 +13,52 @@ Calculate a math expression
 
 §bOperators:§r
 
-- §3First Order:§r
-    *  	Multiplication
-    /  	Division
-    %  	Modulation
+- §3First Order 第一级:§r
+    *  	Multiplication 乘
+    /  	Division 除
+    %  	Modulation 求余
 
-- §3Second Order:§r
-    +  	Addition
-    -  	Subtraction
+- §3Second Order 第二级:§r
+    +  	Addition 加
+    -  	Subtraction 减
 
-- §3Third Order:§r
-    <> 	Logical Not Equal
-    |= 	Bitwise Or Equal
-    &= 	Bitwise And Equal
-    ^= 	Bitwise XOR Equal
-    <= 	Less or Equal
-    >= 	Greater or Equal
-    != 	Not Equal
-    <<	Bitwise Left Shift
-    >>	Bitwise Right Shift
-    =  	Logical Equals
-    >  	Logical Greater Than
-    <  	Logical Less Than
-    && 	Logical AND
-    || 	Logical OR
-    |  	Bitwise OR
-    &  	Bitwise AND
-    ^  	Exclusive OR
-    !  	Logical NOT
+- §3Third Order 第三级:§r
+    <> 	Logical Not Equal 逻辑不等于
+    |= 	Bitwise Or Equal 二进制位或等于
+    &= 	Bitwise And Equal 二进制位与等于
+    ^= 	Bitwise XOR Equal 二进制位异或等于
+    <= 	Less or Equal 小于等于
+    >= 	Greater or Equal 大于等于
+    != 	Not Equal 不等于
+    <<	Bitwise Left Shift 二进制位左移
+    >>	Bitwise Right Shift 俄日禁止位右移
+    =  	Logical Equals 逻辑等于
+    >  	Logical Greater Than 逻辑大于
+    <  	Logical Less Than 逻辑小于
+    && 	Logical AND 逻辑和
+    || 	Logical OR 逻辑或
+    |  	Bitwise OR 二进制位或
+    &  	Bitwise AND 二进制位与
+    ^  	Exclusive OR 二进制异或
+    !  	Logical NOT 逻辑否
+
+- §3Functions 函数:§r
+    pow(x, a) 求x的a次幂(Power a of x)
+    sqrt(x) 开x的二次方(Square root of x)
+    sin(x) = sin x
+    cos(x) = cos x
+    tan(x) = tan x
+
 )"
 
 class CalcCommand : public Command {
 
-    std::string expression;
+    CommandRawText expression;
 
 public:
 
     void execute(const CommandOrigin& ori, CommandOutput& outp) const {
+        std::string expression = this->expression.getText();
         if (expression == "help") {
             outp.success(HELP_TEXT);
             return;
